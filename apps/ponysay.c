c93e3827e7d5bd73883d4f0bd2233b10b429b541
K. Lange <klange@toaruos.org>
2022-03-31 17:28:41 +0900

    Add wrapper to run ponysay in right directory

---
+++ apps/ponysay.c
@@ -0,0 +1,25 @@
+/**
+ * @brief Wrapper tool to launch ponysay
+ *
+ * @warning This is a silly workaround for toolchain limitations.
+ *
+ * @copyright None asserted.
+ */
+#include <unistd.h>
+#include <stdlib.h>
+
+int main(int argc, char * argv[]) {
+	char ** args = malloc(sizeof(char *) * (argc + 3)); /* two for -m ponysay, one for null */
+
+	chdir("/usr/ponysay");
+
+	args[0] = "kuroko";
+	args[1] = "-m";
+	args[2] = "ponysay";
+
+	for (int i = 1; i < argc+1; ++i) {
+		args[i+2] = argv[i];
+	}
+
+	return execvp("kuroko",args);
+}
